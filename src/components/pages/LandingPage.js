c4705e5b7ad31a8337ccecc845b584587a4af0d2
My Tran <my.tran@go1.com>
2022-05-15 22:58:17 +0700

    update UI landing page

--- src/components/pages/LandingPage.js
+++ src/components/pages/LandingPage.js
@@ -1,23 +1,84 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-import '../../App.css'
-import BackgroundImage from '../../assets/images/bg.png'
+import "../../App.css"
 
 export default function LandingPage() {
-    return (
-        <header style={ HeaderStyle }>
-            <p className="main-para text-center">External Portal</p>
-            <div className="buttons text-center">
-                <Link to="/login">
-                    <button className="primary-button">log in</button>
-                </Link>
-                <Link to="/register">
-                    <button className="primary-button" id="reg_btn"><span>register </span></button>
-                </Link>
-            </div>
-        </header>
-    )
+  // React States
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  // User Login info
+  const database = [
+    {
+      username: "test1",
+      password: "123456"
+    },
+  ];
+
+  const errors = {
+    uname: "invalid username",
+    pass: "invalid password"
+  };
+
+  const handleSubmit = (event) => {
+    //Prevent page reload
+    event.preventDefault();
+
+    var { uname, pass } = document.forms[0];
+
+    // Find user login info
+    const userData = database.find((user) => user.username === uname.value);
+
+    // Compare user info
+    if (userData) {
+      if (userData.password !== pass.value) {
+        // Invalid password
+        setErrorMessages({ name: "pass", message: errors.pass });
+      } else {
+        setIsSubmitted(true);
+      }
+    } else {
+      // Username not found
+      setErrorMessages({ name: "uname", message: errors.uname });
+    }
+  };
+
+  // Generate JSX code for error message
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    );
+
+  // JSX code for login form
+  const renderForm = (
+    <div className="form">
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+          <input type="text" name="uname" required />
+          {renderErrorMessage("uname")}
+        </div>
+        <div className="input-container">
+          <input type="password" name="pass" required />
+          {renderErrorMessage("pass")}
+        </div>
+        <div className="button-container">
+          <input type="submit" value="Đăng nhập" />
+        </div>
+      </form>
+    </div>
+  );
+
+  return (
+    <div className="app" style={ HeaderStyle }>
+      <div className="login-form" style={{borderRadius: 25}}>
+        <h1 className="title" style={{marginBottom: 0}}><b>be</b>Lottery</h1>
+        <h2 style={{marginBottom: 0}}>Lottery Ticket Portal</h2> <br/> 
+        Đăng nhập bằng email do Be cung cấp
+        {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
+      </div>
+    </div>
+  );
 }
 
 const HeaderStyle = {
@@ -26,5 +87,8 @@ const HeaderStyle = {
     background: `url('https://wallpaperaccess.com/full/123346.jpg')`,
     backgroundPosition: "center",
     backgroundRepeat: "no-repeat",
-    backgroundSize: "cover"
+    backgroundSize: "cover",
+
 }
+
+
